d0f5ebba11c0bb0796ae0d4560c0725485b04dc6
Omardll001 <omar.dalal.2001@gmail.com>
2025-10-05 05:41:54 +0200

    peeking to check if binary or text

--- tcpservermain.cpp
+++ tcpservermain.cpp
@@ -421,11 +421,8 @@ int main(int argc, char *argv[]) {
         } else if (pid == 0) {
             close(listenfd);
 
-            if (force_binary) {
-                handle_binary_client(connfd);
-            } else {
-                handle_text_client(connfd);
-            }
+            // Always start in text mode, auto-switch to binary if needed
+            handle_text_client(connfd);
 
             close(connfd);
             _exit(0);
@@ -434,7 +431,6 @@ int main(int argc, char *argv[]) {
             // Reap zombie processes
             while (waitpid(-1, NULL, WNOHANG) > 0) {}
         }
-    }
     
     close(listenfd);
     return 0;
